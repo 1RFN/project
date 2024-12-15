0373b398302d0c2b5e97ae2833f07c1f117dc208
F1D02310140 <anisyazwanis79@gmail.com>
2024-12-15 17:55:15 +0800

    anis

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Stack stack = new Stack();
         
+        Barang barang1 = new Barang("001", "Barang A", "Kategori A", 100, 5000);
+        Barang barang2 = new Barang("002", "Barang B", "Kategori B", 50, 7000);
+        Barang barang3 = new Barang("003", "Barang C", "Kategori C", 200, 3000);
+        
+        stack.push(barang1);
+        stack.push(barang2);
+        stack.push(barang3);
+      
+        stack.tampilkan();
+        
+        System.out.println("\nMelakukan pop:");
+        Barang barangPopped = stack.pop();
+        System.out.println("Barang yang di-pop: ID = " + barangPopped.id + ", Nama = " + barangPopped.nama);
+       
+        stack.tampilkan();
+        
+        Barang barang4 = new Barang("004", "Barang D", "Kategori D", 80, 6000);
+        stack.push(barang4);
+        
+        System.out.println("\nMenambahkan barang ke-4 dan menampilkan stack:");
+        stack.tampilkan();
     }
 }
--- Stack.java
+++ Stack.java
@@ -1,3 +1,50 @@
 public class Stack {
+    private Node top;
     
-}
+    private class Node{
+        Barang data;
+        Node next;
+
+        Node(Barang data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public Stack(){
+        top = null;
+    }
+    public void push(Barang barang){
+        Node newNode = new Node(barang);
+        if(top == null){
+            top = newNode;
+        }else{
+            newNode.next = top;
+            top = newNode;
+        }
+    }
+    public Barang pop(){
+        if(top == null){
+            System.out.println("Stack kosong, tidak dapat melakaukan pop");
+            return null;
+        }
+        Barang barang = top.data;
+        top = top.next;
+        return barang;
+    }
+    public void tampilkan(){
+        if(top == null){
+            System.out.println("Stack kosong");
+            return;
+        }
+        Node current = top;
+        System.out.println("Isi stack: ");
+        while(current != null){
+            System.out.println("ID: " + current.data.id + ", nama: " + current.data.nama + 
+            ", kategori: " + current.data.kategori + 
+            ", jumlah stok: " + current.data.jumlah_stok +
+            ", Harga Satuan: " + current.data.harga_satuan);
+            current = current.next;
+        }
+    }
+} 
+
